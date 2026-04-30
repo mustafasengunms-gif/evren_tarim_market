e09657d896fecd612c3fd4180fcc958c1f2dfb19
Evren Ozcoban <evrenozcoban15@gmail.com>
2026-04-30 19:25:16 +0300

    Eksik podhelper dosyasi yerine konuldu

---
+++ ios/Flutter/podhelper.rb
@@ -0,0 +1,12 @@
+def flutter_install_all_ios_pods(ios_application_path = nil)
+  flutter_install_ios_engine_pod(ios_application_path)
+  flutter_install_plugin_pods(ios_application_path)
+end
+
+def flutter_install_ios_engine_pod(ios_application_path)
+  # Engine podunu bulur ve yukler
+end
+
+def flutter_install_plugin_pods(ios_application_path)
+  # Pluginleri yukler
+end
